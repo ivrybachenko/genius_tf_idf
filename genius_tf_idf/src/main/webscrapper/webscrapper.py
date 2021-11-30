c7bac06e2c1045ac2bf3ebc1b4c26ddd07f0d750
ivrybachenko <iv.rybachenko@yandex.ru>
2021-11-30 13:27:58 +0700

    implement load_page_source method
    geckodriver.log file has appeared after test execution, but this file
    is undesirable to index and thus it was added to .gitignore

--- genius_tf_idf/src/main/webscrapper/webscrapper.py
+++ genius_tf_idf/src/main/webscrapper/webscrapper.py
@@ -1,3 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
 class WebScrapper:
+    def __init__(self):
+        self._driver = self._create_driver()
+
     def load_page_source(self, url):
-        return ''
+        self._driver.get(url)
+        return self._driver.page_source
+
+    def _create_driver(self):
+        options = Options()
+        options.headless = True
+        return webdriver.Firefox(options=options)
--- genius_tf_idf/src/test/test_webscrapper.py
+++ genius_tf_idf/src/test/test_webscrapper.py
@@ -2,18 +2,18 @@ import unittest
 from src.main.webscrapper import WebScrapper
 
 class WebscrapperTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.scrapper = WebScrapper()
+
     def test_create_scrapper(self):
-        scrapper = WebScrapper()
-        self.assertIsNotNone(scrapper)
+        self.assertIsNotNone(self.scrapper)
 
     def test_load_page_source_returns_string(self):
-        scrapper = WebScrapper()
-        page_source = scrapper.load_page_source('https://genius.com')
+        page_source = self.scrapper.load_page_source('https://genius.com')
         self.assertTrue(isinstance(page_source, str))
 
     def test_load_page_source_returns_html(self):
-        scrapper = WebScrapper()
-        page_source = scrapper.load_page_source('https://genius.com')
+        page_source = self.scrapper.load_page_source('https://genius.com')
         self.assertTrue("<html>" in page_source)
 
 if __name__ == '__main__':
