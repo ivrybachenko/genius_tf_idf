fb6a86372284cb2a05fd5d4356aa783f3f07f061
ivrybachenko <iv.rybachenko@yandex.ru>
2021-12-04 22:52:03 +0700

    use lyricgenius library

--- genius_tf_idf/src/main/app.py
+++ genius_tf_idf/src/main/app.py
@@ -1,7 +1,7 @@
-from tqdm import tqdm
 import json
 
-from webscrapper import GeniusScrapper
+from lyricsgenius import Genius
+
 
 artists = ['Trent Tomlinson', 'Sting', 'The Parakit',
            'Evanescence', 'Robbie Williams', 'Three Days Grace',
@@ -11,27 +11,18 @@ artists = ['Trent Tomlinson', 'Sting', 'The Parakit',
            'Grace', 'Palastic', 'Eminem', 'Xzibit',
            'No Doubt', 'Bad Meets Evil', 'Snoop Dogg']
 
-
-def load_songs_from_web():
-    scrapper = GeniusScrapper()
-    songs = []
-    for artist in tqdm(artists):
-        songs.extend(scrapper.get_songs(artist, 100))
-    scrapper.dispose()
-    with open('songs.json', 'w') as f:
-        json.dump(songs, f, sort_keys=True)
-    return songs
-
-def load_songs_from_file():
-    with open('songs.json', 'r') as f:
-        return json.load(f)
-
-
-DOWNLOAD = True
 if __name__ == '__main__':
+    token = 'LKKhOfsEcIj6vDyXyQFCIWgoQ1O_SwNq16umN9wAXaSzDOPwOqmk1X1FNNPWH97Z'
+    genius = Genius(token)
     songs = []
-    if DOWNLOAD:
-        songs = load_songs_from_web()
-    else:
-        songs = load_songs_from_file()
+    for artist in artists:
+        artist = genius.search_artist(artist, max_songs=10, sort="title")
+        for song in artist.songs:
+            songs.append({
+                'name': song.full_title,
+                'lyrics': song.lyrics,
+                'artist': song.artist
+            })
+        with open('songs.json', 'w') as f:
+            json.dump(songs, f, sort_keys=True)
     print(f'There are {len(songs)} in total')
