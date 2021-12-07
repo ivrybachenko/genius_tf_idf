515b726dd75fdd69ab5026d4a43279563100519a
ivrybachenko <iv.rybachenko@yandex.ru>
2021-12-07 15:18:55 +0700

    added test on song keys

--- genius_tf_idf/src/test/test_data_loader.py
+++ genius_tf_idf/src/test/test_data_loader.py
@@ -4,9 +4,15 @@ from data import DataLoader
 
 
 class DataLoaderTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.songs = DataLoader.load()
+
     def test_that_count_is_204(self):
-        songs = DataLoader.load()
-        self.assertEqual(204, len(songs))
+        self.assertEqual(204, len(self.songs))
+
+    def test_keys(self):
+        expected_keys = ['artist', 'lyrics', 'name']
+        self.assertEqual(expected_keys, list(self.songs[0].keys()))
 
 
 if __name__ == '__main__':
